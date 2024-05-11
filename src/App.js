3fc66838f506c6180e0628bcfb513d5e2e1da234
JulioChacon <jcchacon3000@gmail.com>
2024-05-11 04:07:51 -0700

    Se crean las rutas utilizando wouter. Se crea contexto para utilizar los Gifs dentro de nuestras paginas.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Detail from './pages/Detail/Detail'
+import Home from './pages/Home/Home'
+import { Route, Link } from 'wouter'
+import SearchResults from './pages/SearchResults/Index'
+import StaticContext from './context/StaticContext'
+import { GifsContextProvider } from './context/GifsContext'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StaticContext.Provider value={{
+      name: 'Julio',
+      suscribete: true
+    }}
+    >
+      <div className='App'>
+        <section className='App-content'>
+
+          <Link to='/'>
+            <figure className='App-logo'>
+              <img alt='Giffy logo' src='/logo.png' /><br />
+            </figure>
+          </Link>
+          <GifsContextProvider>
+            <Route
+              component={Home}
+              path='/'
+            />
+            <Route
+              component={Detail}
+              path='/gif/:id'
+            />
+            <Route
+              component={SearchResults}
+              path='/search/:keyword'
+            />
+          </GifsContextProvider>
+          {/* <Route><ListOfGifs keyword='Toro' /></Route> */}
+          {/* {
+        gifs
+          ? gifs.map(singleGif => <Gif key={singleGif.id} title={singleGif.title} url={singleGif.url} id={singleGif.id} />)
+          : <p>Cargando...</p>
+
+        } */}
+          {/* <button onClick={() => setGifs(DIFERENT_GIFS)}>Change Gif</button> */}
+        </section>
+      </div>
+    </StaticContext.Provider>
+  )
 }
 
-export default App;
+export default App
