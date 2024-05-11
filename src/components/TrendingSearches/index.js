6cc2e75e7131a8524d4df96b6a1d64b0c3995fae
JulioChacon <jcchacon3000@gmail.com>
2024-05-11 04:19:15 -0700

    Se crea componente para cargar los gifs mas buscados solo cuando estos deban aparecer en pantalla.

---
+++ src/components/TrendingSearches/index.js
@@ -0,0 +1,21 @@
+import Spinners from 'components/Spinner'
+import useNearScreen from 'hooks/useNearScreen'
+import React, { Suspense } from 'react'
+
+const TrendingSearches = React.lazy(() => import('./TrendingSearches'))
+
+export default function LazyTrending () {
+  const { isNearScreen, fromRef } = useNearScreen({ distance: '200px' })
+
+  return (
+    <div ref={fromRef}>
+      <Suspense fallback={<Spinners />}>
+        {
+          isNearScreen
+            ? <TrendingSearches />
+            : null
+        }
+      </Suspense>
+    </div>
+  )
+}
